934f7b864b4c071b0e5e7ab3d98e614e7cbcbf38
Adam <szt5238@gmail.com>
2020-05-18 01:26:23 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          
+          
+          
+          <h2>Hello Java</h2>
+
+          
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
