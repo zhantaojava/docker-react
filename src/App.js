939b1a2318ab128685dc39b88e0dcb37019c3016
Adam <szt5238@gmail.com>
2020-05-18 02:05:09 -0400

    add travis

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          
-          
-          <h1>hello</h1>
-          <h2>Hello Java</h2>
-
-          
+          Edit <code>src/App.tsx</code> and save to reload.
+          <h1>Hello World</h1>
         </p>
+        <a className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+
       </header>
     </div>
   );
